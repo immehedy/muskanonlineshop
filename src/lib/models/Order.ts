8738d924ed0c80a5fddc797f0c071400eec93705
Mehedy <dev.mehedy.hassan@gmail.com>
2025-06-14 03:23:16 +0600

    border issue fixed

--- src/lib/models/Order.ts
+++ src/lib/models/Order.ts
@@ -19,11 +19,11 @@ const CartItemSchema = new Schema({
 const ShippingAddressSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: false },
   phone: { type: String, required: true },
   address: { type: String, required: true },
   city: { type: String, required: true },
-  zipCode: { type: String, required: true },
+  zipCode: { type: String, required: false },
   country: { type: String, required: true, default: 'BD' }
 });
 
