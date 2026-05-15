ec3d7d896aed375be220f714394eb7a4f023c8a2
Mehedy <dev.mehedy.hassan@gmail.com>
2026-05-16 02:57:14 +0600

    public site url added

--- src/packages/query/src/apiClient.ts
+++ src/packages/query/src/apiClient.ts
@@ -1,7 +1,5 @@
-// packages/query/src/apiClient.ts
-
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
 
 type RequestOptions = RequestInit;
 
